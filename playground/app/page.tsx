51cabf8bca0371e45aa34741f921767012a5e2a4
jcooky <dennis@habili.ai>
2025-06-11 21:20:41 +0900

    fix: replace default favicon by habiliai

--- playground/app/page.tsx
+++ playground/app/page.tsx
@@ -9,7 +9,6 @@ import {
   CardTitle,
 } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
 import { Textarea } from '@/components/ui/textarea';
 import {
   Dialog,
